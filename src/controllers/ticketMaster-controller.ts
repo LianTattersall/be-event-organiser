f6f8b0a6636d28cc8b2a003b85e93fe93357d1bb
Lian Tattersall <liantattersall@Lians-MBP.lan>
2025-01-31 17:05:56 +0000

    hide ticket master apikey

--- src/controllers/ticketMaster-controller.ts
+++ src/controllers/ticketMaster-controller.ts
@@ -6,7 +6,6 @@ export const getTicketMaster = async (c: Context<{ Bindings: Env }>) => {
 	let requestStr = `https://app.ticketmaster.com/discovery/v2`;
 
 	const path = c.req.routePath.split('/');
-	console.log(path);
 
 	for (let i = 2; i < path.length; i++) {
 		if (c.req.param(path[i].slice(1, path[i].length))) {
@@ -16,10 +15,10 @@ export const getTicketMaster = async (c: Context<{ Bindings: Env }>) => {
 		}
 	}
 
-	requestStr += '?';
+	requestStr += `?apikey=${c.env.TICKET_MASTER_KEY}`;
 	const queries = c.req.queries();
 	for (const key in queries) {
-		requestStr += `${key}=${queries[key][0]}&`;
+		requestStr += `&${key}=${queries[key][0]}`;
 	}
 
 	const data = await axios.get(requestStr);
